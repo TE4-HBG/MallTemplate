ca2d6a23f6d2f8cdcc526249690a7d0f87dac2af
Crazines2 <Adrian.radic@hotmail.com>
2022-09-21 09:26:05 +0200

    change temp started

--- ChangeTemplate.js
+++ ChangeTemplate.js
@@ -1,18 +1,19 @@
 class ChangeTemplate{
-    constructor(Mall_num,  Img_change,  Text_change){
-        var Mall_num_ = "";
-        var Img_change_ = new Array();
-        var Text_change_=""
-        Mall_num_=Mall_num;
-        Img_change_=Array(Img_change);
-        Text_change_=Img_change;
+    constructor(TemplateNum,  Img_change,  Text_change){
+        TemplateNum.this = TemplateNum;
+        Img_change.this = Img_change;
+        Text_change.this = Text_change;
+   
     }
     
 }
 
-function ShowImages(){
-    for (var i =0; i<Img_change.length; i++){
-x
-    }
+function Test(templateNumber) {
+    let templateNum = templateNumber
+}
+
+function ShowTemplate(){
+  const templateToChange = new ChangeTemplate(templateNum,0,document.getElementById("textInput1").value);
+  console.log(templateToChange);
 }
 
