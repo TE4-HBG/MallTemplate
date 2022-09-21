acc5bd050b283f14a10f7cde4a61449b389d4e10
Crazines2 <Adrian.radic@hotmail.com>
2022-09-21 08:53:13 +0200

    class added

---
+++ ChangeTemplate.js
@@ -0,0 +1,18 @@
+class ChangeTemplate{
+    constructor(Mall_num,  Img_change,  Text_change){
+        var Mall_num_ = "";
+        var Img_change_ = new Array();
+        var Text_change_=""
+        Mall_num_=Mall_num;
+        Img_change_=Array(Img_change);
+        Text_change_=Img_change;
+    }
+    
+}
+
+function ShowImages(){
+    for (var i =0; i<Img_change.length; i++){
+x
+    }
+}
+
