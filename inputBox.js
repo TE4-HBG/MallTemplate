6b833619849e45a6cdd2f2f05b0f4a2e51d46ada
Hugo s <hugo.sjoholm@elev.ga.ntig.se>
2022-10-06 13:00:51 +0200

    Can now upload images to the server in JSON format

--- inputBox.js
+++ inputBox.js
@@ -1,72 +1,140 @@
 function PrintInputBorder(Arr) {
-    var Text = document.getElementById('FormsEditable').innerHTML;
+    let Text = "";
     console.log(Arr[0]);
     Text += '<div id="' + Arr[0] + '">' +
-            '<div class="UpdateForms">' +
-            '<button type="button" class="delBtn" onclick=DeleteInputBorder("' + Arr[0] + '")>X</button>' +
-            '<h4>' + Arr[0] + '</h4>' +
-            '<input type="number" id="quantity" name="quantity" min="0" max="15" value="5">' +
-            '<label> Hur många minuter ska sidan visas? (0 = för alltid) </label>' +
-            '<br>';
+        '<div class="UpdateForms">' +
+        '<button type="button" class="delBtn" onclick=DeleteInputBorder("' + Arr[0] + '")>X</button>' +
+        '<h4>' + Arr[0] + '</h4>' +
+        '<input type="number" class="quantity" name="quantity" min="0" max="15" value="5">' +
+        '<label> Hur många minuter ska sidan visas? (0 = för alltid) </label>' +
+        '<br>';
 
-let textAmount = 0;
-let imgAmount = 0;
-for (let index = 0; index < Arr.length; index++) {
-    if (Arr[index] === 'IMG') {
-        imgAmount++;
-        Text += GetImgInput(imgAmount);
+    let textAmount = 0;
+    let imgAmount = 0;
+    for (let index = 0; index < Arr.length; index++) {
+        if (Arr[index] === 'IMG') {
+            imgAmount++;
+            Text += GetImgInput(imgAmount);
+        }
+        if (Arr[index] === 'TEXT') {
+            textAmount++;
+            Text += GetTextInput(textAmount);
+        }
     }
-    if (Arr[index] === 'TEXT') {
-        textAmount++;
-        Text += GetTextInput(textAmount);
-    }
-    if (Arr[index] === 'COUNTDOWN') {
-        Text += GetCountdownInput();
-    }
-}
 
-    
 
-   Text +=	'<button type="button" onclick="Save(' + Arr[0] + ')"><a>Visa</a></button>'+
-            '</div>' +
-           '</div>';
+
+    Text += '<button type="button" onclick=Save("' + Arr[0] + '")><a>Spara</a></button>' +
+        '</div>' +
+        '</div>';
 
     document.getElementById('FormsEditable').innerHTML = Text;
 }
 function GetTextInput(spec) {
-    return  '<label> Text ' + spec + ': </label>' +
-            '<input type="text" id="TEXT" name="' + spec + '" value=""><br>';
+    return '<label> Text ' + spec + ': </label>' +
+        '<input type="text" class="TEXT" name="' + spec + '" value=""><br>';
 }
 function GetImgInput(spec) {
-    return 	'<label> Bild ' + spec + ': </label>'+
-            '<input type="file" id="IMG" name="' + spec + '" accept="image/*">' +
-            '<br>';
+    return '<label> Bild ' + spec + ': </label>' +
+        '<input type="file" class="IMG" name="' + spec + '" accept="image/*">' +
+        '<br>';
 }
 function GetCountdownInput() {
-    return '<label> Countdown: </label>'+
-                '<input type="text"  value="">'+
-                '<label> Tid: </label>'+
-                '<input type="date" id="start" name="trip-start" value="2018-07-22">' +
-                '<br></br>';
+    return '<label> Countdown: </label>' +
+        '<input type="text"  value="">' +
+        '<label> Tid: </label>' +
+        '<input type="date" class="start" name="trip-start" value="2018-07-22">' +
+        '<br></br>';
 }
 
 function specialRNG() {
-   var today = new Date();
-   var today_abs = new Date();
-   today_abs.setHours(0);
-   today_abs.setMinutes(0);
-   today_abs.setSeconds(0);
-   today_secs = (today.getTime() - today_abs.getTime()) / 1000;
-   return today_secs;
+    var today = new Date();
+    var today_abs = new Date();
+    today_abs.setHours(0);
+    today_abs.setMinutes(0);
+    today_abs.setSeconds(0);
+    today_secs = (today.getTime() - today_abs.getTime()) / 1000;
+    return today_secs;
 
 }
 
 
-function Save(x) {
-   console.log('tmp');
-   console.log(x);
-   let parent = document.getElementById(x);
-   console.log(parent.childNodes);
+async function Save(x) {
+
+    let inputBox = document.getElementById(x);
+    let allTextInput;
+    let minutes;
+    let jsonObject;
+    let allImgInput;
+
+    switch (x) {
+        case 'Template1':
+            allTextInput = inputBox.getElementsByClassName("TEXT");
+            allImgInput = inputBox.getElementsByClassName("IMG");
+            minutes = inputBox.getElementsByClassName('quantity');
+            jsonObject = {
+                "text1": allTextInput[0].value,
+                "image1": { type: allImgInput[0].files[0].type, data: await allImgInput[0].files[0].text() }
+            };
+
+
+            send(new Template(minutes[0].value, x, jsonObject));
+
+            break;
+        case 'Template2':
+            allTextInput = inputBox.getElementsByClassName("TEXT");
+            minutes = inputBox.getElementsByClassName('quantity');
+            jsonObject = {
+                "text1": allTextInput[0].value,
+                "text2": allTextInput[1].value
+            }
+
+            send(new Template(minutes[0].value, x, jsonObject));
+
+            break;
+        case 'Template3':
+            allImgInput = inputBox.getElementsByClassName("IMG");
+            minutes = inputBox.getElementsByClassName('quantity');
+            jsonObject = {
+                "image1": { type: allImgInput[0].files[0].type, data: await allImgInput[0].files[0].text() }
+            };
+
+
+            send(new Template(minutes[0].value, x, jsonObject));
+
+            break;
+        case 'Template4':
+            allTextInput = inputBox.getElementsByClassName("TEXT");
+            allImgInput = inputBox.getElementsByClassName("IMG");
+            minutes = inputBox.getElementsByClassName('quantity');
+            jsonObject = {
+                "text1": allTextInput[0].value,
+                "image1": { type: allImgInput[0].files[0].type, data: await allImgInput[0].files[0].text() }
+            };
+
+
+            send(new Template(minutes[0].value, x, jsonObject));
+
+            break;
+        case 'Template5':
+            allTextInput = inputBox.getElementsByClassName("TEXT");
+            minutes = inputBox.getElementsByClassName('quantity');
+            jsonObject = {
+                "text1": allTextInput[0].value,
+                "text2": allTextInput[1].value
+            }
+
+
+
+            send(new Template(minutes[0].value, x, jsonObject));
+
+            break;
+        case 'Template6':
+            break;
+        default:
+            break;
+    }
+
 
 }
 
--- send.js
+++ send.js
@@ -11,33 +11,31 @@ let tmp4 = [];
 
 function addX() {
     let jsonObject = {
-        "content": [
+        "content": 
             {
-                "text1": "Hej Linn, Text 1"
-            },
-            {
-                "text2": "detta är text 2"
-            },
-            {
-                "countdown": [
+                "text1": "Hej Linn, Text 1",
+            
+                "text2": "detta är text 2",
+            
+                "countdown": 
                     {
                         "text": "Studentetn",
                         "date": "2023-06-10"
                     }
-                ]
             }
-        ]
     }
-    let mall1 = new Template(2,1,jsonObject)
+    let mall1 = new Template('2',1,jsonObject)
 
-    tmp4.push(mall1)
+    tmp4.push(mall1);
+    send(JSON.stringify(tmp4));
 }
 
 
 function send(data) {
     const val = socket.readyState == WebSocket.OPEN;
     if(val) {
-        socket.send(data);
+        console.log(data);
+        socket.send(JSON.stringify(data));
     }
     return val;
 }
@@ -48,3 +46,5 @@ function send(data) {
 
 //Note to self
 //KÖR FUNCTIONEN addX() OM DEN RETURNAR UNDEFINEN SKA DEN HA FUNKAT, KOLLA SEN SERVERN GENOM PUTTY OM DET KOM FRAM
+
+
